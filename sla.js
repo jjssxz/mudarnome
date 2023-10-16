7c1b28c0f351c1d39a2c784c4c58305cf3680a06
jjssxz <139500926+jjssxz@users.noreply.github.com>
2023-10-16 08:41:59 -0300

    Add files via upload

---
+++ sla.js
@@ -0,0 +1,5 @@
+function js_style() {
+    document.querySelector('#text').style.color = 'red';
+    document.querySelector('#text').style.fontSize = '25px';
+    document.querySelector('#text').style.fontFamily = 'sans-serif'
+}
